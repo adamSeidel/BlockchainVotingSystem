05989f7007955bcebd926ebccd4d0c672f8bb8f0
Adam Seidel <adamSeidel@users.noreply.github.com>
2025-02-25 14:38:47 +0000

    Automated Contract Deployment and Linking with Frontend

--- blockchain/scripts/StartElection.js
+++ blockchain/scripts/StartElection.js
@@ -1,10 +1,8 @@
-function encodeCandidateNames(candidateNames) {
-    return candidateNames.map(name => ethers.encodeBytes32String(name));
-}
+const path = require('path');
 
 async function main() {
     const [deployer] = await ethers.getSigners();
-    console.log("Deploying election contract with the account:", deployer.address);
+    console.log("Deploying election contract with the account:", await deployer.getAddress());
 
     const accountBalance = await deployer.provider.getBalance(deployer.address);
     console.log("Account balance:", accountBalance.toString());
@@ -18,13 +16,44 @@ async function main() {
         'Reform UK',
         'Green Party'
     ];
-
     const encodedCandidates = encodeCandidateNames(candidates);
 
     const token = await Token.deploy(encodedCandidates);
-
     await token.waitForDeployment();
 
     console.log("Election contract deployed to:", await token.getAddress());
+
+    saveFrontendFiles(token);
 }
+
+function encodeCandidateNames(candidateNames) {
+    return candidateNames.map(name => ethers.encodeBytes32String(name));
+}
+
+function saveFrontendFiles(token) {
+    const fs = require('fs');
+    const contractsDir = path.join(__dirname, '..', 'frontend', 'src', 'contracts');
+
+    if (!fs.existsSync(contractsDir)) {
+        fs.mkdirSync(contractsDir);
+    }
+
+    fs.writeFileSync(
+        path.join(contractsDir, 'contract-address.json'),
+        JSON.stringify({ Election: token.address }, undefined, 2)
+    );
+
+    const TokenArtifact = artifacts.readArtifactSync('Election');
+
+    fs.writeFileSync(
+        path.join(contractsDir, 'Election.json'),
+        JSON.stringify(TokenArtifact, null, 2)
+    );
+}
+
 main()
+    .then(() => process.exit(0))
+    .catch(error => {
+        console.error(error);
+        process.exit(1);
+    });
