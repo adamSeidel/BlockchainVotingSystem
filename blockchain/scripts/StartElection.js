5a741efa2508868c4e3c87c291e502bcd24e049a
Adam Seidel <adamSeidel@users.noreply.github.com>
2025-02-25 15:18:48 +0000

    Fixed start script

--- blockchain/scripts/StartElection.js
+++ blockchain/scripts/StartElection.js
@@ -32,7 +32,7 @@ function encodeCandidateNames(candidateNames) {
 
 function saveFrontendFiles(token) {
     const fs = require('fs');
-    const contractsDir = path.join(__dirname, '..', 'frontend', 'src', 'contracts');
+    const contractsDir = path.join(__dirname, '..', '..', 'frontend', 'src', 'contracts');
 
     if (!fs.existsSync(contractsDir)) {
         fs.mkdirSync(contractsDir);
