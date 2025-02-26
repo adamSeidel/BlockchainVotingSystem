56ef172264a8eae717971ba1e9f2fc53ac1e1d55
Adam Seidel <adamSeidel@users.noreply.github.com>
2025-02-26 17:19:04 +0000

    Added display of voters who have not yet voted

--- frontend/src/components/VotersWhoHaveNotVoted.js
+++ frontend/src/components/VotersWhoHaveNotVoted.js
@@ -1,18 +1,22 @@
+import { useContract } from "../context/ContractProvider";
+
 const VotersWhoHaveNotVoted = () => {
+    const { votersWhoHaveNotVoted } = useContract();
+
     return (
         <>
             <h1>Display Voters That Have Not Voted</h1>
             <div style={{ borderStyle: "solid"}}>
             <h3 style={{ fontWeight: "normal"}}>Addresses listed below have <strong style={{ color: "red" }}>not yet voted</strong> in this election</h3>
             </div>
-            {/* {votersWhoHaveNotVoted.map((address) => {
+            {votersWhoHaveNotVoted.map((address) => {
             return (
                 <div style={{ padding: '1rem 0'}}>
                 <hr></hr>
                 {address}
                 </div>
             );
-            })} */}
+            })}
         </>
     );
 };
