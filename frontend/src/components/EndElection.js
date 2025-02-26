8b1103a82c5a57715b299229af2978c293033cb6
Adam Seidel <adamSeidel@users.noreply.github.com>
2025-02-26 17:29:37 +0000

    Added the ability to end the election

--- frontend/src/components/EndElection.js
+++ frontend/src/components/EndElection.js
@@ -1,4 +1,19 @@
+import { useContract } from "../context/ContractProvider";
+
+const endElection = async (token) => {
+    try {
+        await token.endElection();
+        console.log("The election has been ended successfully");
+        document.getElementById("endElectionMessage").innerHTML = "The election has been ended successfully";
+    } catch (error) {
+        var errorMessage = error.reason;
+        document.getElementById("endElectionMessage").innerHTML = "Error: " + errorMessage;
+    }
+}
+
 const EndElection = () => {
+    const { token } = useContract();
+
     return (
         <>
             <h1>End the Election</h1>
@@ -6,8 +21,7 @@ const EndElection = () => {
             <h3 style={{ fontWeight: "normal"}}>Only the <strong style={{ color: "red" }}>UK Electoral Commission</strong> with a blockchain address of <strong style={{ color: "red" }}>xxx</strong> can end the election</h3>
             </div>
             <div style={{ borderStyle: "solid", padding: '1rem 0', marginTop: "-1.5px"}}>
-            {/* <button onClick={endElection}>End Election</button> */}
-            <button>End Election</button>
+            <button onClick={() => endElection(token)}>End Election</button>
             <h3 id="endElectionMessage"> </h3>
             </div>
         </>
