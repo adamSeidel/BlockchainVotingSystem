e44bd9509a6e7bc2bdae50d14afb674ed6a2e81b
Adam Seidel <adamSeidel@users.noreply.github.com>
2025-04-22 20:49:40 +0100

    Full test plan with example wikipedia simulation

--- blockchain/test/AMS.js
+++ blockchain/test/AMS.js
@@ -690,3 +690,425 @@ describe("Additional Member System - End Election", function () {
             .to.emit(election, "ElectionEnded");
     })
 })
+
+describe("Additional Member System - Calculate Election Results", function () {
+    it("Election results cannot be calculated twice", async function () {
+        [admin, voter, voter2, voter3, voter4] = await ethers.getSigners();
+
+        Election = await ethers.getContractFactory("AMS", admin);
+        election = await Election.deploy(1);
+
+        await election.waitForDeployment();
+
+        // Start the election
+        await election.startElection();
+
+        // End the election
+        await election.endElection();
+        
+        await expect(election.calculateElectionResults())
+            .to.be.revertedWith("The election results have already been calculated so it is not possible to perform this action");
+    })
+
+    it("Should correclty calculate the winner for a single constituency", async function () {
+        [admin, voter, voter2, voter3, voter4] = await ethers.getSigners();
+
+        Election = await ethers.getContractFactory("AMS", admin);
+        election = await Election.deploy(1);
+
+        await election.waitForDeployment();
+        
+        // Add a constituency for testing purposes
+        const constituency1 = ethers.encodeBytes32String("Harrogate");
+        await election.addConstituency(constituency1);
+
+        // Add 3 candidates to the test constituency
+        const candidate1 = ethers.encodeBytes32String("Conservative");
+        await election.addConstituencyCandidate(candidate1, candidate1, constituency1)
+
+        const candidate2 = ethers.encodeBytes32String("Labour");
+        await election.addConstituencyCandidate(candidate2, candidate2, constituency1)
+
+        const candidate3 = ethers.encodeBytes32String("Reform");
+        await election.addConstituencyCandidate(candidate3, candidate3, constituency1)
+
+        // Add 5 elgibile voters for testing purposes
+        await election.addVoter(admin.address, constituency1)
+        await election.addVoter(voter.address, constituency1)
+        await election.addVoter(voter2.address, constituency1)
+        await election.addVoter(voter3.address, constituency1)
+        await election.addVoter(voter4.address, constituency1)
+
+        // Start the election
+        await election.startElection();
+
+        // Cast vote for Conservative Party as admin voter
+        await expect(election.castVote(candidate1, candidate1))
+            .to.emit(election, "VoteCast")
+            .withArgs(admin.address);
+
+        // Cast vote for Conservative Party as voter
+        await expect(election.connect(voter).castVote(candidate1, candidate1))
+            .to.emit(election, "VoteCast")
+            .withArgs(voter.address);
+
+        // Cast vote for Labour Party as voter2
+        await expect(election.connect(voter2).castVote(candidate2, candidate2))
+        .to.emit(election, "VoteCast")
+        .withArgs(voter2.address);
+
+        // Cast vote for Reform Party as vote3
+        await expect(election.connect(voter3).castVote(candidate3, candidate3))
+        .to.emit(election, "VoteCast")
+        .withArgs(voter3.address);
+
+        // End election
+        await expect(election.endElection())
+            .to.emit(election, "ConstituencyWinner")
+            .withArgs(constituency1, candidate1, candidate1)
+    })
+
+    it("Should correctly calculate the election winner - Wikipedia AMS Example", async function () {
+        // Test of an example AMS election from
+        // https://en.wikipedia.org/wiki/Additional-member_system
+        [admin, voter, voter2, voter3, voter4] = await ethers.getSigners();
+
+        Election = await ethers.getContractFactory("AMS", admin);
+        election = await Election.deploy(100);
+
+        await election.waitForDeployment();
+
+        const party1 = ethers.encodeBytes32String("Conservative");
+        const party2 = ethers.encodeBytes32String("Labour Party");
+        const party3 = ethers.encodeBytes32String("Liberal");
+        const party4 = ethers.encodeBytes32String("Reform");
+
+        // Add 70 constituencies
+        for (let i = 0; i < 70; i++) {
+            // Name of the ith constituency
+            let constituencyName = ethers.encodeBytes32String("Constituency" + i);
+            // Add the constituency
+            await election.addConstituency(constituencyName)
+
+            // Add the 4 candidates to the ith constituency
+            await election.addConstituencyCandidate(party1, party1, constituencyName);
+            await election.addConstituencyCandidate(party2, party2, constituencyName);
+            await election.addConstituencyCandidate(party3, party3, constituencyName);
+            await election.addConstituencyCandidate(party4, party4, constituencyName);
+        }
+
+        // Conservative voters
+        const conservativeVoters = []
+        // Add 5 voters to the first 53 constituencies
+        for (let i = 0; i < 53; i++) {
+            for (let j = 0; j < 5; j++) {
+                const voter = ethers.Wallet.createRandom();
+                conservativeVoters.push(voter);
+
+                // Fund the voter waller with some Ether
+                const tx = await admin.sendTransaction({
+                    to: voter.address,
+                    value: ethers.parseEther("0.005"),
+                });
+                await tx.wait();
+
+                // Add the voter to the election
+                constituencyName = ethers.encodeBytes32String("Constituency" + i)
+                await expect(election.addVoter(voter.address, constituencyName))
+                    .to.emit(election, "VoterAdded")
+                    .withArgs(voter.address, constituencyName);
+            }
+        }
+        // Add 36 voters to the 54th constituency
+        for (let j = 0; j < 36; j++) {
+            const voter = ethers.Wallet.createRandom();
+            conservativeVoters.push(voter);
+
+            // Fund the voter waller with some Ether
+            const tx = await admin.sendTransaction({
+                to: voter.address,
+                value: ethers.parseEther("0.005"),
+            });
+            await tx.wait();
+
+            // Add the voter to the election
+            constituencyName = ethers.encodeBytes32String("Constituency" + 53)
+            await expect(election.addVoter(voter.address, constituencyName))
+                .to.emit(election, "VoterAdded")
+                .withArgs(voter.address, constituencyName);
+        }
+
+        // Labour Voters
+        const labourVoters = []
+        // Add 26 votes to each of the following constituencies 55th - 64th
+        for (let i = 0; i < 11; i++) {
+            for (let j = 0; j < 26; j++) {
+                const voter = ethers.Wallet.createRandom();
+                labourVoters.push(voter);
+
+                // Fund the voter waller with some Ether
+                const tx = await admin.sendTransaction({
+                    to: voter.address,
+                    value: ethers.parseEther("0.005"),
+                });
+                await tx.wait();
+
+                // Add the voter to the election
+                constituencyName = ethers.encodeBytes32String("Constituency" + (i+54))
+                await expect(election.addVoter(voter.address, constituencyName))
+                    .to.emit(election, "VoterAdded")
+                    .withArgs(voter.address, constituencyName);
+            }
+        }
+        // Add an extra voter to the 65th constituency
+        voter = ethers.Wallet.createRandom();
+        labourVoters.push(voter);
+
+        // Fund the voter waller with some Ether
+        let tx = await admin.sendTransaction({
+            to: voter.address,
+            value: ethers.parseEther("0.005"),
+        });
+        await tx.wait();
+
+        // Add the voter to the election
+        constituencyName = ethers.encodeBytes32String("Constituency" + 64)
+        await expect(election.addVoter(voter.address, constituencyName))
+            .to.emit(election, "VoterAdded")
+            .withArgs(voter.address, constituencyName);
+
+        // Liberal Voters
+        const liberalVoters = []
+        // Add 11 voters to each of the following constituencies 55th - 62nd
+        for (let i = 0; i < 8; i++) {
+            for (let j = 0; j < 11; j++) {
+                const voter = ethers.Wallet.createRandom();
+                liberalVoters.push(voter);
+
+                // Fund the voter waller with some Ether
+                const tx = await admin.sendTransaction({
+                    to: voter.address,
+                    value: ethers.parseEther("0.005"),
+                });
+                await tx.wait();
+
+                // Add the voter to the election
+                constituencyName = ethers.encodeBytes32String("Constituency" + (i+54))
+                await expect(election.addVoter(voter.address, constituencyName))
+                    .to.emit(election, "VoterAdded")
+                    .withArgs(voter.address, constituencyName);
+            }
+        }
+        // Add 3 voters to the 63rd constituency
+        for (let j = 0; j < 3; j++) {
+            const voter = ethers.Wallet.createRandom();
+            liberalVoters.push(voter);
+
+            // Fund the voter waller with some Ether
+            const tx = await admin.sendTransaction({
+                to: voter.address,
+                value: ethers.parseEther("0.005"),
+            });
+            await tx.wait();
+
+            // Add the voter to the election
+            constituencyName = ethers.encodeBytes32String("Constituency" + 62)
+            await expect(election.addVoter(voter.address, constituencyName))
+                .to.emit(election, "VoterAdded")
+                .withArgs(voter.address, constituencyName);
+        }
+
+        // Reform voters
+        const reformVoters = []
+        // Add 4 reform voters to each of the following constituencies 66th - 69th
+        for (let i = 0; i < 5; i++) {
+            for (let j = 0; j < 4; j++) {
+                const voter = ethers.Wallet.createRandom();
+                reformVoters.push(voter);
+
+                // Fund the voter waller with some Ether
+                const tx = await admin.sendTransaction({
+                    to: voter.address,
+                    value: ethers.parseEther("0.005"),
+                });
+                await tx.wait();
+
+                // Add the voter to the election
+                constituencyName = ethers.encodeBytes32String("Constituency" + (i+65))
+                await expect(election.addVoter(voter.address, constituencyName))
+                    .to.emit(election, "VoterAdded")
+                    .withArgs(voter.address, constituencyName);
+            }
+        }
+        // Add an extra reform voter to the 70th constituency
+        voter = ethers.Wallet.createRandom();
+        reformVoters.push(voter);
+
+        // Fund the voter waller with some Ether
+        tx = await admin.sendTransaction({
+            to: voter.address,
+            value: ethers.parseEther("0.005"),
+        });
+        await tx.wait();
+
+        // Add the voter to the election
+        constituencyName = ethers.encodeBytes32String("Constituency" + 69)
+        await expect(election.addVoter(voter.address, constituencyName))
+            .to.emit(election, "VoterAdded")
+            .withArgs(voter.address, constituencyName);
+
+        // Start the election
+        await election.startElection();
+
+        // Elect 54 conservative constituencies with 5 party conservative votes
+        // in each of the following constituencies 1st - 53rd and 36 votes in
+        // the 54th constituency
+        let conservativeIndex = 0;
+        for (let i = 0; i < 53; i++) {
+            for (let j = 0; j < 5; j++) {
+                const voterWallet = conservativeVoters[conservativeIndex].connect(ethers.provider);
+
+                await expect(election.connect(voterWallet).castVote(party1, party1))
+                    .to.emit(election, "VoteCast")
+                    .withArgs(voterWallet.address);
+
+                conservativeIndex += 1;
+            }
+        }
+        // Add 36 votes to the 54th constituency
+        for (let i = 0; i < 36; i++) {
+            const voterWallet = conservativeVoters[conservativeIndex].connect(ethers.provider);
+
+                await expect(election.connect(voterWallet).castVote(party1, party1))
+                    .to.emit(election, "VoteCast")
+                    .withArgs(voterWallet.address);
+
+                conservativeIndex += 1;
+        }
+
+        // Elect 11 labour constituencies with 26 party labour votes in each of
+        // the following constituencies 55th - 64th and 27 votes in the 65th
+        // constituency
+        let labourIndex = 0;
+        for (let i = 0; i < 11; i++) {
+            for (let j = 0; j < 26; j++) {
+                const voterWallet = labourVoters[labourIndex].connect(ethers.provider);
+
+                await expect(election.connect(voterWallet).castVote(party2, party2))
+                    .to.emit(election, "VoteCast")
+                    .withArgs(voterWallet.address);
+
+                labourIndex += 1;
+            }
+        }
+        // Add an extra vote to the 65th constituency to take the total to 27 votes
+        let voterWallet = labourVoters[labourIndex].connect(ethers.provider);
+
+        await expect(election.connect(voterWallet).castVote(party2, party2))
+            .to.emit(election, "VoteCast")
+            .withArgs(voterWallet.address);
+
+        labourIndex += 1;
+
+        // Add 91 liberal votes without electing any candidates to acheive
+        // popular representation of 13% by adding 11 votes to the 55th - 62nd
+        // constituencies and 3 votes to the 63rd constituency
+        let liberalIndex = 0;
+        for (let i = 0; i < 8; i++) {
+            for (let j = 0; j < 11; j++) {
+                const voterWallet = liberalVoters[liberalIndex].connect(ethers.provider);
+
+                await expect(election.connect(voterWallet).castVote(party3, party3))
+                    .to.emit(election, "VoteCast")
+                    .withArgs(voterWallet.address);
+
+                liberalIndex += 1;
+            }
+        }
+        // Add 3 liberal votes to the 63rd constituency
+        for (let j = 0; j < 3; j++) {
+            const voterWallet = liberalVoters[liberalIndex].connect(ethers.provider);
+
+                await expect(election.connect(voterWallet).castVote(party3, party3))
+                    .to.emit(election, "VoteCast")
+                    .withArgs(voterWallet.address);
+
+                liberalIndex += 1;
+        }
+
+        // Elect 5 reform seats with 4 votes in the 66th - 69th constituencies
+        // and 5 votes in the 70th constituency
+        let reformIndex = 0;
+        for (let i = 0; i < 5; i++) {
+            for (let j = 0; j < 4; j++) {
+                const voterWallet = reformVoters[reformIndex].connect(ethers.provider);
+
+                await expect(election.connect(voterWallet).castVote(party4, party4))
+                    .to.emit(election, "VoteCast")
+                    .withArgs(voterWallet.address);
+
+                reformIndex += 1;
+            }
+        }
+        // Add an extra reform vote to the 70th constituency
+        voterWallet = reformVoters[reformIndex].connect(ethers.provider);
+
+        await expect(election.connect(voterWallet).castVote(party4, party4))
+            .to.emit(election, "VoteCast")
+            .withArgs(voterWallet.address);
+
+        reformIndex += 1;
+
+        // End the election
+        tx = await election.endElection();
+        const result = await tx.wait();
+        const eventLog = result.logs;
+
+        // Election Ended Event
+        expect(eventLog[0].fragment.name).to.equal("ElectionEnded");
+
+        // All Constituency Winners Calcualted Event
+        expect(eventLog[71].fragment.name).to.equal("AllConstituencyWinnersCalculated");
+
+        // Additional Seats Allocated Event
+        expect(eventLog[72].fragment.name).to.equal("AdditionalSeatsAllocated");
+        expect(eventLog[72].args[0]).to.equal(party2);
+        expect(eventLog[72].args[1]).to.equal(22);
+
+        // Additional Seats Allocated Event
+        expect(eventLog[73].fragment.name).to.equal("AdditionalSeatsAllocated");
+        expect(eventLog[73].args[0]).to.equal(party3);
+        expect(eventLog[73].args[1]).to.equal(7);
+
+        // All Additional Seats Allocated Event
+        expect(eventLog[74].fragment.name).to.equal("AllAdditionalSeatsAllocated");
+
+        // Election Results Calculated Event
+        expect(eventLog[75].fragment.name).to.equal("ElectionResultsCalculated");
+
+        // Party Results Event
+        expect(eventLog[76].fragment.name).to.equal("PartyResults");
+        expect(eventLog[76].args[0]).to.equal(party1);
+        expect(eventLog[76].args[1]).to.equal(54);
+
+        // Party Results Event
+        expect(eventLog[77].fragment.name).to.equal("PartyResults");
+        expect(eventLog[77].args[0]).to.equal(party2);
+        expect(eventLog[77].args[1]).to.equal(33);
+
+        // Party Results Event
+        expect(eventLog[78].fragment.name).to.equal("PartyResults");
+        expect(eventLog[78].args[0]).to.equal(party3);
+        expect(eventLog[78].args[1]).to.equal(7);
+
+        // Party Results Event
+        expect(eventLog[79].fragment.name).to.equal("PartyResults");
+        expect(eventLog[79].args[0]).to.equal(party4);
+        expect(eventLog[79].args[1]).to.equal(5);
+
+        // Election Winner
+        expect(eventLog[80].fragment.name).to.equal("ElectionWinner");
+        expect(eventLog[80].args[0]).to.equal(party1);
+        expect(eventLog[80].args[1]).to.equal(54);
+    })
+})
